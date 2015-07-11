8416fb1185e0a33ad5c2045a94a6631fb2ede406
domwakeling <domwakeling@gmail.com>
2015-07-11 19:07:30 +0100

    Version 1.2
    Tidying up:
    - abstracted update_time() so it is called on load
    - improved gpath_centre_rotate() removing need for manual adjustments
    - improved code for placing 'frame' of rotbitmaplayer

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 #include <pebble.h>
-#include "gpathcentrerotate.h"
 
 /*****************************************************/
 /***************** DEFINE CONSTANTS ******************/	
@@ -10,7 +9,7 @@
 #define SCREEN_HEIGHT 168
 
 /* centre of rotation */
-#define ROT_CENTRE_X 72
+#define ROT_CENTRE_X 71
 #define ROT_CENTRE_Y 84
 
 /* top "circle" */
@@ -19,12 +18,21 @@
 #define CIRCLE_FILL_COLOUR GColorWhite
 
 /* seconds hand */
-#define SECONDS_STROKE_COLOUR GColorBlack
-#define SECONDS_FILL_COLOUR GColorWhite
+#define SECONDS_BG_COLOUR GColorBlack
+#define SECONDS_LINE_COLOUR GColorWhite
+#define SECS_LONG 56
+#define SECS_SHORT 23
+#define SECS_WIDTH 1			// stroked 3-wide so that 1 is visible, x-1, x+1; some problems with code ...
 	
 /* hand colours */
 #define HANDS_STROKE_COLOUR GColorBlack
 #define HANDS_FILL_COLOUR GColorWhite
+	
+/* hand sizes */
+#define HR_LONG 44
+#define MIN_LONG 63
+#define HANDS_SHORT	10
+#define HANDS_WIDTH 3				// stroked 7-wide so that 5 is visible; x-3, x+3
 	
 /* seconds paddle */
 #define SECS_IC_X 4
@@ -47,12 +55,12 @@ TextLayer *temp_text_layer;
 
 /* paths, fonts & bitmaps */
 GFont eurostile_font;
-GPath *s_hand_path, *m_hand_path, *h_hand_path;
+GPath *s_hand_path, *s_hand_path_in, *m_hand_path, *h_hand_path;
 GBitmap *seconds_paddle, *watch_dial, *dial_text;
 
 /* variables */
 char temp_buff[] = "00";
-bool drawing_hands = false;
+bool paddle_located = false;
 int hours, minutes, seconds, hours_display;
 int hours_angle = -1;			// set to -1 as default so we draw on load
 int minutes_angle = -1;		// set to -1 as default so we draw on load
@@ -62,24 +70,45 @@ int minutes_angle = -1;		// set to -1 as default so we draw on load
 /*********** DEFINE PATH INFOS & POINTS **************/	
 /*****************************************************/
 
-/* 3-pixel wide rectangle for the seconds hand, stroked at 1px so 1px visible */
+/* 3-pixel wide rectangle for the seconds hand, filled to make the background */
 GPathInfo s_hand_info = {
 	.num_points = 4,
-	.points = (GPoint[]) { {71,28} , {73,28} , {73,107} , {71,107} }
+	.points = (GPoint[]) { {-SECS_WIDTH, -SECS_LONG} , {SECS_WIDTH, -SECS_LONG} , {SECS_WIDTH, SECS_SHORT} , {-SECS_WIDTH, SECS_SHORT} }
 };
 
+/* line for the "filled" seconds hand */
+GPathInfo s_hand_in_info = {
+	.num_points = 2,
+	.points = (GPoint[]) { {0, -SECS_LONG} , {0, SECS_SHORT} }
+};
 
 /* 7-pixel wide rectangle for the minutes hand, stroked at 1px so 5px visible */
+/* hand is now drawn with pivot point at (0,0) for ease of rotation & movement */
 GPathInfo m_hand_info = {
 	.num_points = 4,
-	.points = (GPoint[]) { {69,21} , {75,21} , {75,94} , {69,94} }
+	.points = (GPoint[]) { { -HANDS_WIDTH, -MIN_LONG} , {HANDS_WIDTH, -MIN_LONG}, {HANDS_WIDTH, HANDS_SHORT} , {-HANDS_WIDTH, HANDS_SHORT} }
 };
 
 /* 7-pixel wide rectangle for the hour hand, stroked at 1px so 5px visible */
+/* hand is now drawn with pivot point at (0,0) for ease of rotation & movement */
 GPathInfo h_hand_info = {
 	.num_points = 4,
-	.points = (GPoint[]) { {69,40} , {75,40} , {75,94} , {69,94} }
+	.points = (GPoint[]) { {-HANDS_WIDTH, -HR_LONG} , {HANDS_WIDTH, -HR_LONG} , {HANDS_WIDTH, HANDS_SHORT} , {-HANDS_WIDTH, HANDS_SHORT} }
 };
+
+
+/*****************************************************/
+/***************** HELPER FUNCTIONS ******************/	
+/*****************************************************/
+
+void gpath_centre_rotate(GPath *path, int deg, GPoint point) {
+	// set the rotation
+	int32_t angle = deg * TRIG_MAX_ANGLE / 360;
+	gpath_rotate_to(path, angle);
+	
+	//gpath_move_to(path, trans);
+	gpath_move_to(path, point);
+}
 
 
 /*****************************************************/
@@ -87,44 +116,38 @@ GPathInfo h_hand_info = {
 /*****************************************************/
 
 void hours_update_proc(Layer *l, GContext *ctx) {
-	if(drawing_hands) {
-		// set paths to new angle
-		GPoint rot_point = (GPoint) {ROT_CENTRE_X, ROT_CENTRE_Y};
-		gpath_centre_rotate(h_hand_path, hours_angle, rot_point);
-		// draw and fill the hour hand
-		graphics_context_set_stroke_color(ctx, HANDS_STROKE_COLOUR);
-		graphics_context_set_fill_color(ctx, HANDS_FILL_COLOUR);
-		gpath_draw_filled(ctx, h_hand_path);
-		gpath_draw_outline(ctx, h_hand_path);
-		
-	}
+	// set paths to new angle
+	GPoint rot_point = (GPoint) {ROT_CENTRE_X, ROT_CENTRE_Y};
+	gpath_centre_rotate(h_hand_path, hours_angle, rot_point);
+	// draw and fill the hour hand
+	graphics_context_set_stroke_color(ctx, HANDS_STROKE_COLOUR);
+	graphics_context_set_fill_color(ctx, HANDS_FILL_COLOUR);
+	gpath_draw_filled(ctx, h_hand_path);
+	gpath_draw_outline(ctx, h_hand_path);
 }
 
-void minutes_update_proc(Layer *l, GContext *ctx) {
-	if(drawing_hands) {
-		
-		// set paths to new angle
-		GPoint rot_point = (GPoint) {ROT_CENTRE_X, ROT_CENTRE_Y};
-		gpath_centre_rotate(m_hand_path, minutes_angle, rot_point);
-		
-		// draw and fill the minute hand
-		graphics_context_set_stroke_color(ctx, HANDS_STROKE_COLOUR);
-		graphics_context_set_fill_color(ctx, HANDS_FILL_COLOUR);
-		gpath_draw_filled(ctx, m_hand_path);
-		gpath_draw_outline(ctx, m_hand_path);
-	}
+void minutes_update_proc(Layer *l, GContext *ctx) {	
+	// set paths to new angle
+	GPoint rot_point = (GPoint) {ROT_CENTRE_X, ROT_CENTRE_Y};
+	gpath_centre_rotate(m_hand_path, minutes_angle, rot_point);
+	// draw and fill the minute hand
+	graphics_context_set_stroke_color(ctx, HANDS_STROKE_COLOUR);
+	graphics_context_set_fill_color(ctx, HANDS_FILL_COLOUR);
+	gpath_draw_filled(ctx, m_hand_path);
+	gpath_draw_outline(ctx, m_hand_path);
 }
 
 void seconds_update_proc(Layer *l, GContext *ctx) {
-	if(drawing_hands) {
-		int rot_deg = seconds * (360 / 60);
-		GPoint rot_point = (GPoint) {ROT_CENTRE_X, ROT_CENTRE_Y};
-		gpath_centre_rotate(s_hand_path, rot_deg, rot_point);
-		graphics_context_set_stroke_color(ctx, SECONDS_STROKE_COLOUR);
-		graphics_context_set_fill_color(ctx, SECONDS_FILL_COLOUR);
-		gpath_draw_filled(ctx, s_hand_path);
-		gpath_draw_outline(ctx, s_hand_path);
-	}
+	// set paths to new angle
+	int rot_deg = seconds * (360 / 60);
+	GPoint rot_point = (GPoint) {ROT_CENTRE_X, ROT_CENTRE_Y};
+	gpath_centre_rotate(s_hand_path, rot_deg, rot_point);
+	gpath_centre_rotate(s_hand_path_in, rot_deg, rot_point);
+	// draw and fill background and hand
+	graphics_context_set_stroke_color(ctx, SECONDS_LINE_COLOUR);
+	graphics_context_set_fill_color(ctx, SECONDS_BG_COLOUR);
+	gpath_draw_filled(ctx, s_hand_path);
+	gpath_draw_outline(ctx, s_hand_path_in);
 }
 
 void circle_update_proc(Layer *l, GContext *ctx) {
@@ -135,14 +158,22 @@ void circle_update_proc(Layer *l, GContext *ctx) {
 	graphics_draw_circle(ctx, GPoint(ROT_CENTRE_X, ROT_CENTRE_Y), 1);
 }
 
-
 void paddle_layer_update() {
-	GRect r;
+	// if we haven't already moved the paddle, do so
+	if(!paddle_located) {
+		// get the current frame
+		GRect r;
+		r = layer_get_frame((Layer *)paddle_layer);
+		// set the origin using this info ...
+		r.origin.x = (ROT_CENTRE_X - r.size.w/2);
+		r.origin.y = (ROT_CENTRE_Y - r.size.h/2);
+		layer_set_frame((Layer *)paddle_layer, r);	
+		// change the bool
+		paddle_located = true;
+	}
+	
+	// now rotate the paddle
 	int32_t seconds_angle = seconds * TRIG_MAX_ANGLE / 60;
-	r = layer_get_frame((Layer *)paddle_layer);
-	r.origin.x = (SCREEN_WIDTH - r.size.w)/2;
-	r.origin.y = (SCREEN_HEIGHT - r.size.h)/2;
-	layer_set_frame((Layer *)paddle_layer, r);
 	rot_bitmap_layer_set_angle(paddle_layer, seconds_angle);
 }
 
@@ -163,14 +194,15 @@ int get_hours_angle() {
 }
 
 /*****************************************************/
-/******************* TICK HANDLER ********************/	
+/************ TICK HANDLER & UPDATE TIME *************/	
 /*****************************************************/
 
-/* tick handler - update the hands */
-void tick_handler(struct tm * tick_time, TimeUnits units_changed) {
-
-	// as soon as tick_handler starts, set the hands to be visible
-	drawing_hands = true;
+/* main update_time routine, run at load and then when tick_timer 'beeps' */
+void update_time() {
+	
+	// get a temporary tm struct with time
+	time_t temp = time(NULL);
+	struct tm *tick_time = localtime(&temp);
 	
 	// get time into storage
 	hours = (int)tick_time->tm_hour;
@@ -208,12 +240,14 @@ void tick_handler(struct tm * tick_time, TimeUnits units_changed) {
 	// draw seconds hand
 	layer_mark_dirty(seconds_layer);
 	
-	// un-hide the paddle first time we draw - stops paddle showing in random position/angle on load
-	bool paddle_hidden = layer_get_hidden((Layer *)paddle_layer);
-	if(paddle_hidden) layer_set_hidden((Layer *)paddle_layer, false);
+	// draw the paddle
 	paddle_layer_update();
 }
 
+/* tick_handler, only job is to update the time */
+void tick_handler(struct tm * tick_time, TimeUnits units_changed) {
+	update_time();
+}
 
 /*****************************************************/
 /*************** MAIN WINDOW HANDLERS ****************/	
@@ -241,6 +275,7 @@ static void main_window_load(Window *w) {
 		
 	// init our gpaths
 	s_hand_path = gpath_create(&s_hand_info);
+	s_hand_path_in = gpath_create(&s_hand_in_info);
 	m_hand_path = gpath_create(&m_hand_info);
 	h_hand_path = gpath_create(&h_hand_info);
 	
@@ -283,9 +318,8 @@ static void main_window_load(Window *w) {
 	layer_add_child(w_layer, (Layer *)paddle_layer);
 	layer_add_child(w_layer, circle_layer);
 	
-	// hide the paddle layer until we've moved it!
-	layer_set_hidden((Layer *)paddle_layer, true);
-	
+	// update the time as we finish loading
+	update_time();	
 }
 
 static void main_window_unload(Window *w) {
@@ -300,6 +334,7 @@ static void main_window_unload(Window *w) {
 	
 	// destroy gpaths & gbitmaps
 	gpath_destroy(s_hand_path);
+	gpath_destroy(s_hand_path_in);
 	gpath_destroy(m_hand_path);
 	gpath_destroy(h_hand_path);
 	gbitmap_destroy(seconds_paddle);
